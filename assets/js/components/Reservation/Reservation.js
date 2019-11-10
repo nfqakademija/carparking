7de9a57188b1011924a3c81992d7674f314874c0
Lukas Laurinavicius <lukaslaurina@gmail.com>
2019-11-10 15:30:17 +0200

    buttons

--- assets/js/components/Reservation/Reservation.js
+++ assets/js/components/Reservation/Reservation.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import Button from '../UI/Button/Button';
+
 import '../../../css/components/Reservation/Reservation.scss';
 
 const reservation = (props) => {
+
+    const buttonStyle = {width:'50%'}
+
     return (
         <div className='Reservation_container'>
             <div className='Reservation_header'>
@@ -13,10 +18,10 @@ const reservation = (props) => {
                     <svg className='Reservation_svg'>
                         <circle className='Reservation_circleGreen' cx="50%" cy="50%" r="30%"/>
                         <circle className='Reservation_circleRed' cx="50%" cy="50%" r="30%"
-                            strokeDasharray="60% 188.4%" />
+                            strokeDasharray="142% 188.4%" />
                         <text className='Reservation_text' x="50%" y="50%">A34</text>
                     </svg>
-                    <span>Button should be here</span>
+                    <Button text="ask for switch" classname='Button_danger' buttonStyle={buttonStyle}/>
             </div>
         </div>
     )
