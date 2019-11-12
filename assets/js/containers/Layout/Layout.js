e47c382c950d9de4ed3acfca316366987e5bf73f
lukas <stresas2@gmail.com>
2019-11-12 20:12:25 +0200

    Updated: css color classes
    Added:
    icons: icono
    docs: https://saeedalipoor.github.io/icono/
    
    Grid,
    resizable header navbar,
    resizable sidebar,
    mobile sidebar
    some extra stuff,
    
    still need improve design...

--- assets/js/containers/Layout/Layout.js
+++ assets/js/containers/Layout/Layout.js
@@ -1,32 +1,100 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+
 
 import Background from '../../components/UI/Background/Background';
+
+const meniu =
+    <div className="hide-text ml-2">
+        <div className="nav-item">
+            <a className="nav-link" href="#services">My Profile</a>
+        </div>
+        <div className="nav-item">
+            <a className="nav-link" href="#contact">Reserve</a>
+        </div>
+        <div className="nav-item ">
+            <a className="nav-link" href="#contact">Watch Users</a>
+        </div>
+    </div>;
+
+const openMeniu = () => {
+    document.getElementById("mySidenav").style.width = "100%";
+};
+
+const closeMeniu = () => {
+    document.getElementById("mySidenav").style.width = "0%";
+};
 
 const layout = (props) => (
     <>
         <Background/>
-        <header>
-            {/* there should be header (in our case without any links) */}
-            <div style={{backgroundColor:'#f1a177', height:'4em', color:'white', fontWeight:'900', fontSize:'1.5em', boxShadow:' 0px 0px 10px 0px grey'}}>
-                
-                    <span style={{float:'left', marginLeft: '3em', marginTop:'1em'}}>NFQ parking</span>
-                    <span style={{float:'right', marginRight: '3em', marginTop:'1em'}}>Vardenis Pavardenis</span>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-12 navheight">
+                    <div className="fixed-top">
+                        <nav className="navbar navbar-dark bg-dark px-2 m-0 navheight">
+                            <div className="w-100">
+                                <div className="d-flex justify-content-between">
+                                    <div>
+                                        <a className="h1 textLightGrey"
+                                        >NFQ parking</a
+                                        >
+                                    </div>
+                                    <div className="align-self-center d-flex">
+                                        <div className="d-none d-md-block align-self-center">
+                                            <i className="textLightGrey h4 align-middle m-0">Jonas Jonaitis</i>
+                                        </div>
+                                        <div>
+                                            <i
+                                                className="icono-power textLightGrey mx-4"
+                                            > </i>
+                                        </div>
+                                        <div onClick={openMeniu}>
+                                            <i
+                                                className="icono-hamburger h3 text-white d-md-none"
+                                            > </i>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="sidenav p-0 m-0 bg_lightGrey" id="mySidenav">
+                                    <div className="container-fluid w-100 p-0 p-2">
+                                        <div className="d-flex justify-content-between">
+                                            <div className="align-self-center">
+                                                <p className="m-0 pl-0 h4">
+                                                    Menu
+                                                </p>
+                                            </div>
+                                            <div>
+                                                <a
+                                                    className="mb-0 h2"
+                                                    onClick={closeMeniu}
+                                                >×</a
+                                                >
+                                            </div>
+                                        </div>
+                                    </div>
+                                    {meniu}
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
             </div>
-        </header>
-        <main style={{height:'100%'}}>
-            {/* this should be main part with navbar and content (content part need props children inside!!) */}
-            <nav style={{width:"14em", backgroundColor:"#fadfd1", height:"95vh", position:'absolute', zIndex:'2', boxShadow:' 0px 10px 10px 0px grey'}}>
-                <p style={{marginTop:"0", paddingTop:"1em", textAlign:"center", fontSize:'1.3em', color:"#99A0A6"}}>MENU</p>
-                <hr/>
-                <div style={{display:'flex', flexDirection:'column'}}>
-                    <Link to='/home' > Home </Link>
-                    <Link to='/users'> Users </Link>
-                    <Link to='/logout'> Logout </Link>
-                </div>   
-            </nav>
-            {props.children}
-        </main>
+        </div>
+
+        <div className="container-fluid mainheight">
+            <div className="row h-100">
+                <div className="bg_grey d-none d-md-block col-0 col-md-4 col-lg-3 p-0 ">
+                        <div className="d-none d-md-block col-0 col-md-4 col-lg-3 position-fixed p-0">{meniu}</div>
+                </div>
+                <div className="col-12 col-md-8 col-lg-9">
+                    <div className="container-fluid">
+                        {props.children}
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
 )
 
