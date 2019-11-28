94292487159ca1f5903162f3b2dca9b4bc5ddeaf
Kestas <55563304+kestas144@users.noreply.github.com>
2019-11-28 04:11:11 +0200

    fosrest and jmsserializer bundles

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
