5efdee7920e27945c98c996be30a52d2912f73f7
Lukas Laurinavicius <lukaslaurina@gmail.com>
2019-11-30 17:58:38 +0200

    added cases by clicked button

--- assets/js/components/UI/PopUp/PopUp.js
+++ assets/js/components/UI/PopUp/PopUp.js
@@ -8,7 +8,7 @@ const popUp = (props) => {
         left: props.left,
         width: props.width,
         transform: props.translate ?'translateY(0)': 'translateY(-200px)',
-        height: props.translate? '10%': '0'
+        height: props.translate? 'auto': '0'
     }
 
     popupType = {
@@ -18,17 +18,20 @@ const popUp = (props) => {
 
     let statusText = <span></span>
             
-
     if(props.type){
+        const date = new Date(props.type.date)
+        const dateString = `${date.toLocaleDateString('en-EN', {weekday:'short'}).toUpperCase()}
+                            ${date.toLocaleDateString('en-EN', {month:'short'}).toUpperCase()}
+                            ${date.getDate()}`
         switch (props.type.type){
             case 'success':
-                statusText = <span> Are you sure you want to reserve a parking spot for {props.date}</span>
+                statusText = <span style={{textTransform:'uppercase'}}> Are you sure you want to reserve a parking spot for {dateString}</span>
                 break
             case 'danger':
-                statusText = <span> Are you sure you want to cancel your parking spot for {props.date}</span>
+                statusText = <span style={{textTransform:'uppercase'}}> Are you sure you want to cancel your parking spot for {dateString}</span>
                 break
             case 'neutral':
-                statusText = <span> Are you sure you want to ask Lukas to swap parking spaces for  {props.date}</span>
+                statusText = <span style={{textTransform:'uppercase'}}> Are you sure you want to ask Lukas to swap parking spaces for  {dateString}</span>
                 break
         }
     }
@@ -37,8 +40,10 @@ const popUp = (props) => {
         <>
             <div className='PopUp_container' style={popupType} >
                 {statusText}
-                <button>1</button>
-                <button>2</button>     
+                <div>
+                    <button>1</button>
+                    <button>2</button> 
+                </div>    
             </div>
                     
         </>
