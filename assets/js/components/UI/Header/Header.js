c1257572ba7c9b25f1ce708b3a1a4072db052d65
lukas <stresas2@gmail.com>
2019-12-04 16:13:49 +0200

    Improve design for webMenu, mobileMenu, header. Changed CSS

--- assets/js/components/UI/Header/Header.js
+++ assets/js/components/UI/Header/Header.js
@@ -8,7 +8,7 @@ import {openMobileMenu, closeMobileMenu} from "../../../store/actions/main";
 
 
 const header = (props) => (
-        <div className="container-fluid box-shadow" >
+        <div className="container-fluid shadow" >
             <div className="row">
                 <div className="col-12 navheight">
                     <div className="fixed-top">
@@ -16,7 +16,9 @@ const header = (props) => (
                             <div className="w-100">
                                 <div className="d-flex justify-content-between">
                                     <div>
-                                        <span className="h1 textLightGrey headerText" style={{marginLeft:'2rem'}}>
+
+                                        <span className="textLightGrey headerText" style={{marginLeft:'2rem'}}>
+                                            <i className="mr-4 fas fa-car headerText" style={{color: "orange"}}> </i>
                                             NFQ parking
                                         </span>
                                     </div>
@@ -28,20 +30,20 @@ const header = (props) => (
                                         </div>
                                         <div>
                                             <Link to='/logout' style={{color:"#f8f3f0"}}>
-                                                <i className="icono-power textLightGrey mx-4">
+                                                <i className="fas fa-sign-out-alt h3 textLightGrey mx-4">
                                                 </i>
                                             </Link>
 
                                         </div>
                                         <div onClick={props.openMobileMenu} style={{ cursor: "pointer" }}>
                                             <i
-                                                className="icono-hamburger h3 text-white d-md-none"
+                                                className="fa fa-bars h3 text-white d-md-none mr-2"
                                             > </i>
                                         </div>
                                     </div>
                                 </div>
 
-                                <div className="sidenav p-0 m-0 bg_lightGrey h1 text-nowrap" style={props.mobileMenu ? {width: "100%"} : {width: "0%"}} >
+                                <div className="sidenav p-0 m-0 bg-light h1 text-nowrap" style={props.mobileMenu ? {width: "100%"} : {width: "0%"}} >
                                     <Menu />
                                 </div>
                             </div>
--- assets/js/components/UI/Menu/webMenu.js
+++ assets/js/components/UI/Menu/webMenu.js
@@ -3,28 +3,32 @@ import {NavLink} from "react-router-dom";
 
 
 const webMeniu = (props) => (
-            <div className="d-none bg_lightGrey d-md-block col-0 col-md-4 col-lg-3 p-0 box-shadow h-100">
-                <div className="d-none d-md-block col-0 col-md-4 col-lg-3 position-fixed p-0 mt-2">
-                    <div className="py-2">
-                        <p className=" m-0 h3">
-                            <b>Menu</b>
-                        </p>
+    <div className="d-none bg-light d-md-block col-0 col-md-4 col-lg-3 p-0 shadow h-100">
+        <div className="d-none d-md-block col-0 col-md-4 col-lg-3 position-fixed p-0 mt-2">
+
+            <div className="text-left webMeniu">
+
+                <div className="position-relative mt-1 py-2 linkParent">
+                <span className="ml-4 position-absolute">
+                    <i className="fa fa-check-square-o"></i></span>
+                    <div style={{marginLeft: "80px"}}>
+                        <NavLink to='/home' className="link" onClick={props.onclick}>Reserve</NavLink>
                     </div>
-
-                    <ul className="text-left w3-ul">
-                        <li>
-                            <i className="fa fa-check-square-o"></i>
-                            <NavLink className=" text-dark h2" to='/home' onClick={props.onclick}>Reserve</NavLink>
-                        </li>
-
-                        <li>
-                            <i className="fa fa-users mr-2"></i>
-                            <NavLink className=" text-dark h2" to="/users" onClick={props.onclick}>Watch Users</NavLink>
-                        </li>
-                    </ul>
-
                 </div>
+
+                <hr className="m-0" />
+
+                <div className="position-relative py-2 linkParent">
+                    <span className="ml-4 position-absolute"><i className="fa fa-users"></i></span>
+                    <div style={{marginLeft: "80px"}}>
+                        <NavLink to="/users" className="link" onClick={props.onclick}>Watch Users</NavLink>
+                    </div>
+                </div>
+
             </div>
+
+        </div>
+    </div>
 )
 
 export default webMeniu;
