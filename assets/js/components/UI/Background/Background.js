5c55c31a7a0edaf54df6aa1ba845819282754ede
lukas <stresas2@gmail.com>
2019-12-09 19:25:36 +0200

    test

--- assets/js/components/UI/Background/Background.js
+++ assets/js/components/UI/Background/Background.js
@@ -1,35 +1,24 @@
 import React from 'react';
 
 import '../../../../css/components/UI/Background/Background.scss';
+import car from '../../../../css/cars/cars1.svg';
 
-console.log('aaaa')
+import SVG from 'react-inlinesvg';
+
 const background = (props) => (
     <div className='Background_container'>
         <div className="car-wrapper">
             <div className="car-wrapper_inner">
-                <div className="car_outter">  
+                <div className="car_outter">
                     <div className="car">
                         <div className="body">
-                            <div></div>
+                            <SVG src={car} />
                         </div>
-                        <div className="decos">
-                            <div className="line-bot"></div>
-                            <div className="door">
-                                <div className="handle"></div>
-                                <div className="bottom"></div>
-                        </div>
-                        <div className="window"></div> 
-                        <div className="light"></div>
-                        <div className="light-front"></div>
-                        <div className="antenna"></div>
-                        <div className="ice-cream">
-                            <div className="text">NFQ</div>
-                        </div>  
-                        </div>
+
                         <div>
                             <div className="wheel"></div>
                             <div className="wheel"></div>
-                        </div>    
+                        </div>
                         <div className="wind">
                             <div className="p p1"></div>
                             <div className="p p2"></div>
---
+++ assets/js/components/UI/Background/Car.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Car = (props) => (
+    <div>
+
+    </div>
+)
+
+export default Car;
+
