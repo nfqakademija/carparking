73bb1fc990b128165ddf64523cf9856dbb55621e
lukas <stresas2@gmail.com>
2019-12-10 11:26:19 +0200

    3 cars

--- assets/js/components/UI/Background/Background.js
+++ assets/js/components/UI/Background/Background.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import '../../../../css/components/UI/Background/Background.scss';
-import car from '../../../../css/cars/cars1.svg';
+import car1 from '../../../../css/cars/cars1.svg';
+import car2 from '../../../../css/cars/cars2.svg';
+import car3 from '../../../../css/cars/cars3.svg';
 import wheel from '../../../../css/cars/wheel.svg';
 
 import SVG from 'react-inlinesvg';
@@ -10,28 +12,54 @@ const background = (props) => (
     <div className='Background_container'>
         <div className="car-wrapper">
             <div className="car-wrapper_inner">
-                <div className="car_outter">
-                    <div className="car">
+                <div className="car_outter1">
+                    <div className="car1">
 
-                        <div className="body">
-                            <SVG className="svg" src={car} />
+                        <div className="bodyCar1">
+                            <SVG className="svg1" src={car1}/>
                         </div>
 
-
-                        <div>
-                            <SVG className="wheel1" src={wheel} />
-                            <SVG className="wheel2" src={wheel} />
-                        </div>
-
-
-
-                        <div className="wind">
+                        <div className="wind1">
                             <div className="p p1"></div>
                             <div className="p p2"></div>
                             <div className="p p3"></div>
                             <div className="p p4"></div>
                             <div className="p p5"></div>
                         </div>
+                    </div>
+                </div>
+                <div className="car_outter2">
+                    <div className="car2">
+
+                        <div className="bodyCar2">
+                            <SVG className="svg2" src={car2}/>
+                        </div>
+
+                        <div className="wind2">
+                            <div className="p p1"></div>
+                            <div className="p p2"></div>
+                            <div className="p p3"></div>
+                            <div className="p p4"></div>
+                            <div className="p p5"></div>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="car_outter3">
+                    <div className="car3">
+
+                        <div className="bodyCar3">
+                            <SVG className="svg3" src={car3}/>
+                        </div>
+
+                        <div className="wind3">
+                            <div className="p p1"></div>
+                            <div className="p p2"></div>
+                            <div className="p p3"></div>
+                            <div className="p p4"></div>
+                            <div className="p p5"></div>
+                        </div>
+
                     </div>
                 </div>
             </div>
