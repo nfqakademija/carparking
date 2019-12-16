15fde016c3d6d5e19ed9732c2971f54c208cbdd1
Kestas <55563304+kestas144@users.noreply.github.com>
2019-12-16 20:52:59 +0200

    qfix

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -35,13 +35,4 @@ class LoginController extends AbstractController
 //            $response->headers->clearCookie($cookieName, $cookieData['path'], $cookieData['domain']);
 //        }
     }
-
-//https://accounts.google.com/o/oauth2/v2/
-//auth?scope=openid
-//%20email
-//%20profile&state=7ce1fd224fe5dd87fbc36399098e17cb&response_type=code
-//&approval_prompt=auto&redirect_uri=http%3A%2F%2F127.0.0.1
-//%3A8000%2Fconnect%2Fgoogle%2Fcheck&client_id=45766845940-jmhn5bffb9e9l1coqecms4p7ug237led.apps.googleusercontent.com
-
-
 }
